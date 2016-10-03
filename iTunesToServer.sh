b933c42881c47795e1e3396b31ac7c77c5d62aa8
majorsl <steve@themajorshome.com>
2016-10-03 18:44:37 -0400

    A server and client script to publish iTunes to a webpage.
    This is a client script that reads what is currently playing in your
    iTunes library and pushes it to your server. The server-side script
    then parses the sent data to format it for your page. This is what I
    use on my WordPress blog, but you could tailor it to work with just
    about anything. Read the comments in the scripts.

---
+++ iTunesToServer.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+# Version 2.0.2
+# Script to share what is playing with iTunes to a Web Page. Companion script is iTunes
+# to web. Assumes you have password-less SSH Keys setup between your client(s) & server!
+#
+# 1. It assumes your iTunes library is in the default location. If not, modify the path on
+# line 27.
+# 2. Adjust lines 64 & 65 for where the counterpart script is on your web-server.
+# 3. Use a launchd script to run this at user login: ~/LaunchAgents
+
+# oldinfo/info is the last data sent, if it does not match that triggers the upload to the server.
+oldinfo="empty"
+filemod="empty"
+
+while :
+do
+
+# iTunes Open Status true/false
+itunes=`osascript -e 'tell application "System Events" to set iTunesIsRunning to (name of processes) contains "iTunes"'`
+echo "**diagnostic loop** iTunes Open:" $itunes
+
+	# Get/check iTunes state.
+	if [ "$itunes" = "true" ]; then
+		state=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to player state as string"))'`
+		#-> this is the old way I was doing it, but it would open iTunes if it was closed. Above will check if it is running first. state=`osascript -e 'tell application "iTunes" to player state as string'`
+		sleep 10
+		filemod=`stat -f '%m' ~/Music/iTunes/iTunes\ Library.itl`
+		echo "**file modification $filemod**"
+
+		# When playing: sed statement for smart quotes because a standard ' will confuse BASH in the literal string. Also checking file modification to avoid osascript polling & opening iTunes soon after a recent quit.
+		if [ "$state" = "playing" ]; then
+			if [ "$filemod" != "$oldfilemod" ]; then
+				artist=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to artist of current track as string"))' | sed s/\'/"\\&"\#8217\;/g`
+				track=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to name of current track as string"))' | sed s/\'/"\\&"\#8217\;/g`
+				album=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to album of current track as string"))' | sed s/\'/"\\&"\#8217\;/g`
+				rating=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to rating of current track as string"))'`
+
+			itunesstring="$album\n$artist\n$rating\n$track"
+			info="$artist$track$album"
+			echo "**diagnostic track info-playing $state $artist $track $album $rating**"
+			fi
+			oldfilemod="$filemod"
+			
+		# When not playing.
+		else
+			itunesstring="empty\nempty\n0\nempty"
+			echo "**diagnostic iTunes stopped**"
+			info="empty"
+			filemod="empty"
+		fi
+		
+	# iTunes is not Open. Write not running data the same as if stopped/paused.
+	else
+		sleep 10
+		itunesstring="empty\nempty\n0\nempty"
+		echo "**diagnostic iTunes not Open**"
+		info="empty"
+		filemod="empty"
+	fi
+
+	# Write data if new.
+	if [ "$oldinfo" != "$info" ]; then
+		echo "**diagnostic send to server**"
+		ssh -o ServerAliveCountMax=2 -o ConnectTimeout=30 server.themajorshome.com "printf '%b\n' '$itunesstring' > ~/Sites/tunes/itunesstring.txt"
+		ssh -o ServerAliveCountMax=2 -o ConnectTimeout=30 server.themajorshome.com '~/Scripts/GitHub/iTunesToWebPage/./iTunesToWeb.sh'
+		oldinfo="$info"
+	fi
+
+done
---
+++ iTunesToWeb.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+# Version 2.0.3
+# Script to share what is playing with iTunes to a Web Page. Companion script is
+# iTunesToServer.
+#
+# This script takes the feed from the client and formats it for the blog (currently
+# WordPress) side. The client side script is what evokes this, there is no need for a
+# launchd entry on your server.
+#
+# The last line of this script is where you can modify your output.
+#
+# Set Other Options Here:
+
+# Path to the directory where the companion uploads its data to as well as where the html
+# file this creates will be written & served by your webserver:
+maindata="/Users/majorsl/Sites/tunes/"
+
+# Star rating symbol to use.
+star="&#9733;"
+
+# Directory of Album Artwork. Must be a .jpg and in "Artist Name - Album Name" format and
+# match the artist & album name in your iTunes exactly!
+# Example filename: Gin Blossoms - Outside Looking In.jpg
+# This also needs to be in your site path so it can display the artwork to visitors.
+artwork="/Users/majorsl/Sites/Artwork/"
+
+# Where to put a text file listing missing album art.
+missing="~/Desktop/missingartwork.txt"
+
+# End of Options
+
+cd $maindata
+
+rating=""
+
+IFS=$'\n'
+
+# Array: 0=album 1=artist 2=rating 3=track  Will I need to sanitize with sed "s/\&#39;/\'/" at some point?
+tune=($(cat itunesstring.txt))
+unset IFS
+
+# Cleanup of filenames for artwork file links e.g. smartquotes currently.
+album=`echo "${tune[0]}" | sed s/\&\#8217\;/\'/g`
+artist=`echo "${tune[1]}" | sed s/\&\#8217\;/\'/g`
+
+if [ "${tune[3]}" = "empty" ]; then
+	rm itunesblog.html
+	touch itunesblog.html
+	replace=""
+	exit
+fi
+
+# Assemble star rating.
+xloop="0"
+starnumber=$((${tune[2]} / 20))
+
+while [ "$xloop" -lt "$starnumber" ]; do
+rating="$star""$rating"
+let xloop=$xloop+1
+done
+
+if [ "$starnumber" = "0" ]; then
+	rating=""
+elif [ "$starnumber" > "0" ]; then
+	rating=' I rate it <span style="font-size: 120%;">'$rating'</span>.'
+fi
+
+# Does the artwork exist?
+if [ ! -f "$artwork$artist - $album.jpg" ]; then
+    #No? Better tell someone by slapping a txt file on the server desktop and set a generic!
+    echo "$artist - $album.jpg" >> $missing
+    album="noart"
+    artist="noart"
+fi
+
+#String for Wordpress Blog
+echo '<h3 class="widget-title">Currently Listening To</h3>"'${tune[3]}'" by '${tune[1]}' from the album <i>'${tune[0]}'</i>.'$rating'<br><img src="https://www.themajorshome.com/Artwork/'$artist' - '$album'.jpg" alt="" width="300" border="0">' > itunesblog.html
