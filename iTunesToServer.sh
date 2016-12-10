99f25401051aba9edf091f69d72b61d5dde4a464
majorsl <steve@themajorshome.com>
2016-12-10 12:48:38 -0500

    Use pgrep instead of AppleScript to determine iTunes status. Server variable added.

--- iTunesToServer.sh
+++ iTunesToServer.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-# Version 2.0.4
+# Version 2.0.5
 # Script to share what is playing with iTunes to a Web Page. Companion script is iTunes
 # to web. Assumes you have password-less SSH Keys setup between your client(s) & server!
 #
 # 1. It assumes your iTunes library is in the default location. If not, modify the path on
 # line 27.
-# 2. Adjust lines 64 & 65 for server name & where the counterpart script is on your web-server.
+# 2. Adjust lines 69 & 70 for where the counterpart script is on your web-server.
 # 3. Use a launchd script to run this at user login: ~/LaunchAgents
+
+# Server to send data to.
+server="server3.themajorshome.com"
 
 # oldinfo/info is the last data sent, if it does not match that triggers the upload to the server.
 oldinfo="empty"
@@ -15,14 +18,16 @@ filemod="empty"
 while :
 do
 
-# iTunes Open Status true/false
-itunes=`osascript -e 'tell application "System Events" to set iTunesIsRunning to (name of processes) contains "iTunes"'`
+# iTunes Open Status true / false
+#itunes=`osascript -e 'tell application "System Events" to set iTunesIsRunning to (name of processes) contains "iTunes"'`
+if pgrep -x "iTunes"; then itunes="true"; else itunes="false"; fi
 echo "**diagnostic loop** iTunes Open:" $itunes
 
 	# Get/check iTunes state.
 	if [ "$itunes" = "true" ]; then
-		state=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to player state as string"))'`
-		#-> this is the old way I was doing it, but it would open iTunes if it was closed. Above will check if it is running first. state=`osascript -e 'tell application "iTunes" to player state as string'`
+		#state=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to player state as string"))'`
+		state=`if pgrep -x -q "iTunes"; then osascript -e 'tell application "iTunes" to player state as string'; fi`
+		#-> this is the old way I was doing it, but it would open iTunes if it was closed. Above will double-check if it is running first. state=`osascript -e 'tell application "iTunes" to player state as string'`
 		sleep 10
 		filemod=`stat -f '%m' ~/Music/iTunes/iTunes\ Library.itl`
 		echo "**file modification $filemod**"
@@ -61,8 +66,8 @@ echo "**diagnostic loop** iTunes Open:" $itunes
 	# Write data if new.
 	if [ "$oldinfo" != "$info" ]; then
 		echo "**diagnostic send to server**"
-		ssh -o ServerAliveCountMax=2 -o ConnectTimeout=30 server3.themajorshome.com "printf '%b\n' '$itunesstring' > ~/Sites/tunes/itunesstring.txt"
-		ssh -o ServerAliveCountMax=2 -o ConnectTimeout=30 server3.themajorshome.com '~/Scripts/GitHub/iTunesToWebPage/./iTunesToWeb.sh'
+		ssh -o ServerAliveCountMax=2 -o ConnectTimeout=30 $server "printf '%b\n' '$itunesstring' > ~/Sites/tunes/itunesstring.txt"
+		ssh -o ServerAliveCountMax=2 -o ConnectTimeout=30 $server '~/Scripts/GitHub/iTunesToWebPage/./iTunesToWeb.sh'
 		oldinfo="$info"
 	fi
 
