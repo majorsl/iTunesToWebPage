ad190660f7c745ee2af7959a22bea61479636962
majorsl <steve@themajorshome.com>
2016-12-11 10:02:40 -0500

    Unused code in comments clean up.

--- iTunesToServer.sh
+++ iTunesToServer.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-# Version 2.0.8
+# Version 2.0.9
 # Script to share what is playing with iTunes to a Web Page. Companion script is iTunes
 # to web. Assumes you have password-less SSH Keys setup between your client(s) & server!
 #
-# 1. It assumes your iTunes library is in the default location. If not, modify the path on
-# line 27.
-# 2. Use a launchd script to run this at user login: ~/LaunchAgents
-
+# Use a launchd script to run this at user login: ~/LaunchAgents
+#
+# --Options--
 # Path to your iTunes Library (if yours is default, don't modify.)
 ituneslibrary="/Users/majorsl/Music/iTunes/iTunes Library.itl"
 # Server to send data to.
@@ -15,6 +14,7 @@ server="server3.themajorshome.com"
 serverdata="~/Sites/tunes"
 # Path on server where counterpart iTunesToWeb script is located.
 serverscript="~/Scripts/GitHub/iTunesToWebPage"
+# --End Options--
 
 # oldinfo/info is the last data sent, if it does not match that triggers the upload to the server.
 oldinfo="empty"
@@ -24,18 +24,13 @@ while :
 do
 
 # iTunes Open Status true / false
-#itunes=`osascript -e 'tell application "System Events" to set iTunesIsRunning to (name of processes) contains "iTunes"'`
 if pgrep -x "iTunes"; then itunes="true"; else itunes="false"; fi
 echo "**diagnostic loop** iTunes Open:" $itunes
 
 	# Get/check iTunes state.
 	if [ "$itunes" = "true" ]; then
-		#state=`osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to player state as string"))'`
 		state=$(if pgrep -x -q "iTunes"; then osascript -e 'tell application "iTunes" to player state as string'; fi)
-		#-> this is the old way I was doing it, but it would open iTunes if it was closed. Above will double-check if it is running first. state=`osascript -e 'tell application "iTunes" to player state as string'`
 		sleep 10
-		#filemod=$(stat -f "%m" "$ituneslibrary")
-		#echo "**file modification $filemod**"
 
 		# When playing: sed statement for smart quotes because a standard ' will confuse BASH in the literal string. Also checking file modification to avoid osascript polling & opening iTunes soon after a recent quit.
 		if [ "$state" = "playing" ]; then
@@ -48,15 +43,13 @@ echo "**diagnostic loop** iTunes Open:" $itunes
 			itunesstring="$album\n$artist\n$rating\n$track"
 			info="$artist$track$album"
 			echo "**diagnostic track info-playing $state $artist $track $album $rating**"
-			fi
-			#oldfilemod="$filemod"
-			
+			fi			
+
 		# When not playing.
 		else
 			itunesstring="empty\nempty\n0\nempty"
 			echo "**diagnostic iTunes stopped**"
 			info="empty"
-			#filemod="empty"
 		fi
 		
 	# iTunes is not Open. Write not running data the same as if stopped/paused.
@@ -64,8 +57,7 @@ echo "**diagnostic loop** iTunes Open:" $itunes
 		sleep 10
 		itunesstring="empty\nempty\n0\nempty"
 		echo "**diagnostic iTunes not Open**"
-		info="empty"
-		#filemod="empty"
+		info="empty"		#filemod="empty"
 	fi
 
 	# Write data if new.
